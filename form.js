4862c89e1c2b930fa439fe462f0b7485b4c4873a
Yashashav <krishnasaxena271@gmail.com>
2021-09-17 18:39:10 +0530

    Adding form.js to open or hide Today's List Form

---
+++ form.js
@@ -0,0 +1,11 @@
+form = document.getElementById("AddItemForm") // Getting Element or Form
+
+// To Open Form
+function openForm() {
+    form.style.display = "block"
+}
+
+// To Close Form
+function closeForm() {
+    form.style.display = "none"
+}
